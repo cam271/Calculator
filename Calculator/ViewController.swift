36f5a6d0b1e5413301a4b29fc50f93e41c0d4821
Messenger, C <messenger_c@ymail.com>
2017-02-09 01:35:37 -0500

    Added IBoutlet result and IBAction numericButtons. Created buttons in UI

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var result: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: Actions
+    @IBAction func numericButtons(_ sender: UIButton) {
+        result.text = sender.currentTitle
+    }
 
 }
 
